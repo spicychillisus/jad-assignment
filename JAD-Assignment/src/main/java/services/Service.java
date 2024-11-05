2b349db8d03cbc78d27da25c85460468c804c499
spicychillisus <asherben90@gmail.com>
2024-11-06 00:45:35 +0800

    test commit

---
+++ JAD-Assignment/src/main/java/services/Service.java
@@ -0,0 +1,20 @@
+package services;
+
+public class Service {
+	
+	private String name;
+	private String category;
+	private String details;
+	
+	public Service() {
+		
+	}
+	
+	public void setCategory(String cat) {
+		this.category = cat;
+	}
+	
+	public String displayCategory() {
+		return "";
+	}
+}
