6f2d1fb29b10b403c972f12a3af80ce445567dea
spicychillisus <asherben90@gmail.com>
2024-11-12 15:47:24 +0800

    yy

--- JAD-Assignment/src/main/java/config/Config.java
+++ JAD-Assignment/src/main/java/config/Config.java
@@ -4,5 +4,14 @@ public class Config {
 	private String username = "neondb_owner";
 	private String password = "5Dgya7KkJQor";
 	
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	
 	
 }
